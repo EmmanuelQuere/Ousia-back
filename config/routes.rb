89272e06bf4d4cdeae9351b3b082896bb392eb58
Ariane <pikabooariane@hotmail.fr>
2021-06-15 17:23:21 +0300

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :carts
   resources :items
   devise_for :users
+
+  resources :users, only: [:update, :show]
+  get 'profile', to: 'users#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
