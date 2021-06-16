af2a14954346ac8e991ba28a725d8bbb36e241e0
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-06-16 15:52:51 +0200

    feature / Adding cart_item controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
+  after_create :create_cart
   has_one :cart
   has_many :orders
   # Include default devise modules. Others available are:
@@ -8,4 +9,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+
+  private
+
+    def create_cart
+      Cart.create(user_id: self.id)
+    end
 end
