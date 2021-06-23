7a937c9a9da262dd3cee60b5ab4c0aa288001614
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-23 17:52:55 +0200

    FEATURE create new model/migration/controller to save users emails

---
+++ db/migrate/20210623152854_create_newsletter_subscribers.rb
@@ -0,0 +1,9 @@
+class CreateNewsletterSubscribers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :newsletter_subscribers do |t|
+      t.string :email
+      t.boolean :active, default: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_085758) do
+ActiveRecord::Schema.define(version: 2021_06_23_152854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,13 @@ ActiveRecord::Schema.define(version: 2021_06_23_085758) do
     t.text "description"
     t.integer "price"
     t.integer "stock"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "newsletter_subscribers", force: :cascade do |t|
+    t.string "email"
+    t.boolean "active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
