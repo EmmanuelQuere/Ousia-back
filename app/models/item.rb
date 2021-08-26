7a5ae1d3eb93d6b5a34c2451df452b8c0e8d4503
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-08-26 17:47:28 +0200

    update // change text limit

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   has_many :carts, through: :cart_items
   has_many_attached :images
   validates :name, presence: true, length: { in: 3..50 }
-  validates :description, presence: true, length: { maximum: 400 }
+  validates :description, presence: true, length: { maximum: 2000 }
   validates :price, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 1000}
   validates :stock, numericality: {greater_than_or_equal_to: 0, only_integer: true}
 
